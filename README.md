279a147977d77ca19fad7e772a9eb938d434d957
Michael Manis <michaelmanis@tutanota.com>
2018-08-05 13:12:58 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# /vrg/ VRChat worlds website
+### Maintaining
+ - The site is meant to be self-sufficent in terms of HTML/Javascript/CSS.
+ - If you want to add a feature fork the repository and submit a Pull Request.
+ - If you want to add/remove/edit maps, submit a Pull Request or ask someone to do it for you.
+### About the config.json file
+ - The config.json allows the site to be easily updated without having to write more HTML.
+ - Every world can (but does not need to) have a `name`, `author`, `world_id`, and `thumbnail`.
+ - The `world_id` should _not_ contain `vrchat://`, `id=`, etc.. See the existing config for examples.
+ - Thumbnails should be `512x512` or at least an approximately square image. 
+ - New categories can be added by adding a new key to the `worlds` dictionary.
+   - For example:
+```json
+{
+  "worlds": {
+    "public": [
+      ...
+    ],
+    "unlisted": [
+      ...
+    ],
+    "myNewCategory": [
+      ...
+    ]
+  }
+}
+```
+The site will update automatically when the changes are merged.
---
+++ js/index.js
@@ -0,0 +1,64 @@
+const VRCHAT_PREFIX = 'vrchat://launch?ref=vrchat&id=';
+
+function worldClicked(world) {
+    let win = window.open(world.getAttribute('data-url'), '_blank');
+    if (win)
+        win.focus();
+}
+
+function loadJSON(file_path, callback) {
+    var req = new XMLHttpRequest();
+    req.overrideMimeType("application/json");
+    req.open('GET', file_path, true);
+    req.onreadystatechange = function () {
+        if (req.readyState === 4 && req.status === 200) {
+            callback(JSON.parse(req.responseText));
+        }
+    };
+    req.send(null);
+}
+
+function parseWorlds(json) {
+    let world_dict = {};
+    let world_config = json["worlds"];
+    for (let category in world_config) {
+        if (!world_dict.hasOwnProperty(category)) world_dict[category] = [];
+
+        for (let world in world_config[category]) {
+            let world_data = world_config[category][world];
+            let new_btn = document.createElement('button');
+            new_btn.setAttribute('class', 'btn');
+            new_btn.setAttribute('onClick', 'worldClicked(this)');
+            new_btn.setAttribute('data-url',(world_data.hasOwnProperty('world_id')) ? VRCHAT_PREFIX + world_data['world_id'] : VRCHAT_PREFIX +  "unknown_id");
+
+            let world_name = (world_data.hasOwnProperty('name')) ? world_data['name'] : 'Unnamed World';
+            let world_author = (world_data.hasOwnProperty('author')) ? " - " + world_data['author'] : " - Anonymous";
+            let world_thumb = (world_data.hasOwnProperty('thumbnail')) ? 'thumbnails/' + world_data['thumbnail'] : 'thumbnails/placeholder.png';
+
+            new_btn.innerHTML += '<img src="' + world_thumb + '" class="thumbnail"><span class="label">' + world_name + world_author + '</span>';
+
+            world_dict[category].push(new_btn);
+        }
+    }
+    return world_dict;
+}
+
+function worldsJSONReady(json) {
+    let world_dict = parseWorlds(json);
+    for (let category in world_dict) {
+        document.body.innerHTML +=
+            '<div class="title-container">' +
+            '<span class="title-text">' + category + '</span>' +
+            '</div>' +
+            '<div id="' + category + '-button-area" class="button-area flex-column">' +
+            '</div>';
+        let btn_area = document.getElementById(category + '-button-area');
+        for (let world in world_dict[category]) {
+            btn_area.appendChild(world_dict[category][world]);
+        }
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function (event) {
+    loadJSON('config.json', worldsJSONReady);
+});
